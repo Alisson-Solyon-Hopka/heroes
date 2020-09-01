7126cba7431bc49546ed447d0eff0906d3207166
Alisson-Solyon-Hopka <alisson.sh@hotmail.com>
2020-09-01 15:30:32 -0300

    Angular Material

--- src/app/marvel/marvel.module.ts
+++ src/app/marvel/marvel.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatButtonModule} from '@angular/material/button';
 
 import { ListComponent } from './list/list.component';
 import { HomeComponent } from './home/home.component';
 import { DescricaoComponent } from './descricao/descricao.component';
 
 @NgModule({
-  declarations: [ListComponent, HomeComponent, DescricaoComponent],
+  declarations: [
+    ListComponent,
+    HomeComponent,
+    DescricaoComponent
+  ],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    MatButtonModule
+  ],
+  exports: [
+    MatButtonModule
+  ],
 })
 export class MarvelModule { }
