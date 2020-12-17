e8a037b5ccc7cf9068bc837d5642534eef905631
Alisson-Solyon-Hopka <alisson.sh@hotmail.com>
2020-12-17 12:04:02 -0300

    ajuste HTTPS

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,7 @@ export const environment = {
     ts: 1,
     public_key:  '5e9d6c613a2d7f80d877529f7c99a342',
     hash: 'b3dd7facce62f2338fe982e23ca59832',
-    url_base: 'http://gateway.marvel.com/v1/public/'
+    url_base: 'https://gateway.marvel.com/v1/public/'
   }
 };
 
